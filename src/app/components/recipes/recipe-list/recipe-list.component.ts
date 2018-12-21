6aae17a5103bc76216e31c5a247cd8432508a14b
unknown <dannyfsociety@gmail.com>
2018-12-21 17:10:40 -0400

    Initial Commit -m :v: :relaxed: :bomb:

---
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/models/recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Pasta', 'this is the desc', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2011/2/4/1/RX-FNM_030111-Lighten-Up-012_s4x3.jpg.rend.hgtvcom.616.462.suffix/1382539856907.jpeg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
