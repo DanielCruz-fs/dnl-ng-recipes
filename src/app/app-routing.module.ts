9345d77bda081020dfeecd4b78be9ebbf45e3596
unknown <dannyfsociety@gmail.com>
2018-12-23 12:03:53 -0400

    Communication between Components :octocat: :pizza: :sushi:

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'recipe', component: RecipesComponent },
+  { path: 'shopping', component: ShoppingListComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'recipe'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from 'src/app/models/recipe.model';
 
 @Component({
@@ -8,11 +8,15 @@ import { Recipe } from 'src/app/models/recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Pasta', 'this is the desc', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2011/2/4/1/RX-FNM_030111-Lighten-Up-012_s4x3.jpg.rend.hgtvcom.616.462.suffix/1382539856907.jpeg')
+    new Recipe('Pasta', 'this is the desc', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2011/2/4/1/RX-FNM_030111-Lighten-Up-012_s4x3.jpg.rend.hgtvcom.616.462.suffix/1382539856907.jpeg'),
+    new Recipe('Tofu', 'this is the tofu', 'https://simpleveganblog-lne9w9dshg8v.stackpathdns.com/wp-content/uploads/2019/03/Easy-marinated-tofu-6.jpg')
   ];
+  @Output() recipeSelected = new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit() {
   }
-
+  onRecipeDetail(recipeObject: Recipe) {
+    this.recipeSelected.emit(recipeObject);
+  }
 }
